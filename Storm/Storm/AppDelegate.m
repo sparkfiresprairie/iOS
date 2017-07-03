6280bb7d9df5d615381bc195c30e83dc6a232916
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2017-07-02 23:31:49 -0700

    Exercise 1
    Delete Main.storyboard and add code to
    application:didFinishLaunchingWithOptions

--- Storm/Storm/AppDelegate.m
+++ Storm/Storm/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [_window makeKeyAndVisible];
     return YES;
 }
 
