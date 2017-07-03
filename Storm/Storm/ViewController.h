f45e25b1e4e2c11c8cd803996d2890406b23f40c
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2017-07-02 23:18:17 -0700

    Storm Project Initialized

---
+++ Storm/Storm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Storm
+//
+//  Created by Xingyuan Wang on 7/2/17.
+//  Copyright © 2017 Spark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Storm/Storm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Storm
+//
+//  Created by Xingyuan Wang on 7/2/17.
+//  Copyright © 2017 Spark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
